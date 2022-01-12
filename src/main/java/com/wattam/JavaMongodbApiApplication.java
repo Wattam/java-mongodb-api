bc729837be06a0284882a0bb14e8823cf4f0c5a4
Nalbert Wattam <nalbertwattam@gmail.com>
2022-01-12 19:05:46 -0300

    Initial projected created

---
+++ src/main/java/com/wattam/JavaMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.wattam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMongodbApiApplication.class, args);
+	}
+
+}
